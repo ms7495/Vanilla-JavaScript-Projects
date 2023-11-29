152b3e20fabf11e22679851cb1dae250cdf26144
ms7495 <79163902+ms7495@users.noreply.github.com>
2023-11-29 22:13:25 +0100

    Back to basics, just starting...

---
+++ ColorFlipper/hex.js
@@ -0,0 +1,12 @@
+function change() {
+    let red = Math.floor(Math.random() * 256)
+    let green = Math.floor(Math.random() * 256)
+    let blue = Math.floor(Math.random() * 256)
+    document.getElementById("buttonPart").style.backgroundColor = "rgb(" + red + ',' + green + ',' + blue + ')'
+
+    let redHex = red.toString(16).padStart(2, '0');
+    let greenHex = green.toString(16).padStart(2, '0');
+    let blueHex = blue.toString(16).padStart(2, '0');
+    document.getElementById('hexed').textContent = `#${redHex}${greenHex}${blueHex}`;
+
+}
